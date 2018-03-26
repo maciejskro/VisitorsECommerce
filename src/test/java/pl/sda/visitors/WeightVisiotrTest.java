bff7a16b57145be63c770fc93e587007bc0db8a0
maciejskro <maciejskro@gmail.com>
2018-03-26 23:37:07 +0200

    update description project and structure

--- src/test/java/pl/sda/visitors/WeightVisiotrTest.java
+++ src/test/java/pl/sda/visitors/WeightVisiotrTest.java
@@ -15,8 +15,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-;
-
 
 public class WeightVisiotrTest {
 
